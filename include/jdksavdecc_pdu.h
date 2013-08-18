06765f2d0773ec2781f408c4deae8d4a65cbfc2d
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-08-17 17:02:38 -0700

    Added functions to extract 1722 common stream header fields

--- include/jdksavdecc_pdu.h
+++ include/jdksavdecc_pdu.h
@@ -197,6 +197,83 @@ static inline uint32_t jdksavdecc_subtype_data_set_control_data_length( uint32_t
     return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_CONTROL_DATA_LENGTH_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_CONTROL_DATA_LENGTH_BIT)) & JDKSAVDECC_SUBTYPE_DATA_CONTROL_DATA_LENGTH );
 }
 
+
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_BIT (12)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_WIDTH (1)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_MR (uint32_t)(0x00080000UL)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_MASK (~JDKSAVDECC_SUBTYPE_DATA_STREAM_MR)
+
+static inline uint32_t jdksavdecc_subtype_data_get_mr( uint32_t v )
+{
+   return (v & JDKSAVDECC_SUBTYPE_DATA_STREAM_MR) >> (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_BIT);
+}
+
+static inline uint32_t jdksavdecc_subtype_data_set_mr( uint32_t subtype_data, uint32_t v )
+{
+    return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_MR_BIT)) & JDKSAVDECC_SUBTYPE_DATA_STREAM_MR );
+}
+
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_BIT (14)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_WIDTH (1)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_GV (uint32_t)(0x00020000UL)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_MASK (~JDKSAVDECC_SUBTYPE_DATA_STREAM_GV)
+
+static inline uint32_t jdksavdecc_subtype_data_get_gv( uint32_t v )
+{
+   return (v & JDKSAVDECC_SUBTYPE_DATA_STREAM_GV) >> (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_BIT);
+}
+
+static inline uint32_t jdksavdecc_subtype_data_set_gv( uint32_t subtype_data, uint32_t v )
+{
+    return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_GV_BIT)) & JDKSAVDECC_SUBTYPE_DATA_STREAM_GV );
+}
+
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_BIT (15)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_WIDTH (1)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TV (uint32_t)(0x00010000UL)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_MASK (~JDKSAVDECC_SUBTYPE_DATA_STREAM_TV)
+
+static inline uint32_t jdksavdecc_subtype_data_get_tv( uint32_t v )
+{
+   return (v & JDKSAVDECC_SUBTYPE_DATA_STREAM_TV) >> (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_BIT);
+}
+
+static inline uint32_t jdksavdecc_subtype_data_set_tv( uint32_t subtype_data, uint32_t v )
+{
+    return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_TV_BIT)) & JDKSAVDECC_SUBTYPE_DATA_STREAM_TV );
+}
+
+
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_BIT (22)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_WIDTH (8)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM (uint32_t)(0x0000ff00UL)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_MASK (~JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM)
+
+static inline uint32_t jdksavdecc_subtype_data_get_sequence_num( uint32_t v )
+{
+   return (v & JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM) >> (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_BIT);
+}
+
+static inline uint32_t jdksavdecc_subtype_data_set_sequence_num( uint32_t subtype_data, uint32_t v )
+{
+    return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM_BIT)) & JDKSAVDECC_SUBTYPE_DATA_STREAM_SEQUENCE_NUM );
+}
+
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_BIT (31)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_WIDTH (1)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TU (uint32_t)(0x00000001UL)
+#define JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_MASK (~JDKSAVDECC_SUBTYPE_DATA_STREAM_TU)
+
+static inline uint32_t jdksavdecc_subtype_data_get_tu( uint32_t v )
+{
+   return (v & JDKSAVDECC_SUBTYPE_DATA_STREAM_TU) >> (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_BIT);
+}
+
+static inline uint32_t jdksavdecc_subtype_data_set_tu( uint32_t subtype_data, uint32_t v )
+{
+    return (subtype_data & JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_MASK) | ((v << (31-JDKSAVDECC_SUBTYPE_DATA_STREAM_TU_BIT)) & JDKSAVDECC_SUBTYPE_DATA_STREAM_TU );
+}
+
 /*@}*/
 
 
@@ -339,6 +416,201 @@ ssize_t jdksavdecc_common_control_header_write( struct jdksavdecc_common_control
 
 /*@}*/
 
+
+/** \addtogroup avtp_common_stream_header AVTP common stream header - See IEEE Std 1722-2011 Clause 5.4 */
+/*@{*/
+
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_SUBTYPE_DATA (0)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_ID (4)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_AVTP_TIMESTAMP (12)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_GATEWAY_INFO (16)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_DATA_LENGTH (20)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_PROTOCOL_SPECIFIC_HEADER (22)
+#define JDKSAVDECC_COMMON_STREAM_HEADER_LEN (24)
+
+static inline uint32_t jdksavdecc_common_stream_header_get_subtype_data( void const *base, size_t pos )
+{
+    uint32_t subtype_data;
+    jdksavdecc_uint32_read( &subtype_data, base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_SUBTYPE_DATA );
+    return subtype_data;
+}
+
+static inline void jdksavdecc_common_stream_header_set_subtype_data( uint32_t subtype_data, void *base, size_t pos )
+{
+    jdksavdecc_uint32_set( subtype_data, base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_SUBTYPE_DATA );
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_cd( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_cd(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_cd( uint32_t v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_cd( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_subtype( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_subtype(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_subtype( uint32_t v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_subtype( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+static inline uint32_t jdksavdecc_common_stream_header_get_sv( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_sv(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_sv( uint32_t v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_sv( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_version( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_version(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_version( uint32_t v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_version( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_mr( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_mr(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_mr( int v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_mr( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_gv( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_gv(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_gv( int v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_gv( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_tv( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_tv(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_tv( int v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_tv( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+static inline uint32_t jdksavdecc_common_stream_header_get_sequence_num( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_sequence_num(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_sequence_num( int v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_sequence_num( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+static inline uint32_t jdksavdecc_common_stream_header_get_tu( void const *base, size_t pos )
+{
+    return jdksavdecc_subtype_data_get_tu(jdksavdecc_common_stream_header_get_subtype_data(base,pos));
+}
+
+static inline void jdksavdecc_common_stream_header_set_tu( int v, void *base, size_t pos )
+{
+    uint32_t subtype_data = jdksavdecc_common_stream_header_get_subtype_data(base,pos);
+    subtype_data = jdksavdecc_subtype_data_set_tu( subtype_data, v );
+    jdksavdecc_common_stream_header_set_subtype_data(subtype_data,base,pos);
+}
+
+
+static inline struct jdksavdecc_eui64 jdksavdecc_common_stream_header_get_stream_id( void const *base, size_t pos )
+{
+    return jdksavdecc_eui64_get( base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_ID );
+}
+
+static inline void jdksavdecc_common_stream_header_set_stream_id( struct jdksavdecc_eui64 v, void *base, size_t pos )
+{
+    jdksavdecc_eui64_set(v,base,pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_ID);
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_avtp_timestamp( void const *base, size_t pos )
+{
+    return jdksavdecc_uint32_get( base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_AVTP_TIMESTAMP );
+}
+
+static inline void jdksavdecc_common_stream_header_set_avtp_timestamp( uint32_t v, void *base, size_t pos )
+{
+    jdksavdecc_uint32_set(v,base,pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_AVTP_TIMESTAMP );
+}
+
+
+static inline uint32_t jdksavdecc_common_stream_header_get_gateway_info( void const *base, size_t pos )
+{
+    return jdksavdecc_uint32_get( base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_GATEWAY_INFO );
+}
+
+static inline void jdksavdecc_common_stream_header_set_avtp_gateway_info( uint32_t v, void *base, size_t pos )
+{
+    jdksavdecc_uint32_set(v,base,pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_GATEWAY_INFO );
+}
+
+
+static inline uint16_t jdksavdecc_common_stream_header_get_stream_data_length( void const *base, size_t pos )
+{
+    return jdksavdecc_uint16_get( base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_DATA_LENGTH );
+}
+
+static inline void jdksavdecc_common_stream_header_set_stream_data_length( uint16_t v, void *base, size_t pos )
+{
+    jdksavdecc_uint16_set(v,base,pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_STREAM_DATA_LENGTH );
+}
+
+
+static inline uint16_t jdksavdecc_common_stream_header_get_protocol_specific_header( void const *base, size_t pos )
+{
+    return jdksavdecc_uint16_get( base, pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_PROTOCOL_SPECIFIC_HEADER );
+}
+
+static inline void jdksavdecc_common_stream_header_set_protocol_specific_header( uint16_t v, void *base, size_t pos )
+{
+    jdksavdecc_uint16_set(v,base,pos+JDKSAVDECC_COMMON_STREAM_HEADER_OFFSET_PROTOCOL_SPECIFIC_HEADER );
+}
+
+
+/*@}*/
 
 /** \addtogroup frame raw ethernet frame */
 /*@{*/
