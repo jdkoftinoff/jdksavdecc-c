db1d12df34bb6366c7ff90c147ec2e9ee4c60264
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-09 15:14:40 -0800

    arduino fixups

--- include/jdksavdecc_arduino.h
+++ include/jdksavdecc_arduino.h
@@ -35,70 +35,8 @@
 
 #if defined(__AVR__)
 
-#if !defined(__cplusplus)
-#define inline __inline__
-#endif
-
-typedef int ssize_t;
-
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdbool.h>
-
-#ifndef PRIi8
-#define PRIi8 "hd"
-#endif
-
-#ifndef PRIu8
-#define PRIu8 "hu"
-#endif
-
-#ifndef PRIx8
-#define PRIx8 "hx"
-#endif
-
-#ifndef PRIi16
-#define PRIi16 "d"
-#endif
-
-#ifndef PRIu16
-#define PRIu16 "u"
-#endif
-
-#ifndef PRIx16
-#define PRIx16 "x"
-#endif
-
-#ifndef PRIi32
-#define PRIi32 "ld"
-#endif
-
-#ifndef PRIu32
-#define PRIu32 "lu"
-#endif
-
-#ifndef PRIx32
-#define PRIx32 "lx"
-#endif
-
-#ifndef PRIi64
-#define PRIi64 "ld"
-#endif
-
-#ifndef PRIu64
-#define PRIu64 "lu"
-#endif
-
-#ifndef PRIx64
-#define PRIx64 "lx"
-#endif
-
-#ifndef EXIT_SUCCESS
-#define EXIT_SUCCESS (0)
-#endif
-
-#ifndef EXIT_FAILURE
-#define EXIT_FAILURE (1)
-#endif
+#include "jdksavdecc_avr.h"
+#include "jdksavdecc_descriptor_storage.h"
+#include "jdksavdecc_entity_model.h"
 
 #endif
