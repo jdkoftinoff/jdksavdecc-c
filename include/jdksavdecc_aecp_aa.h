0ac56ee099dd5833540338f9959ee8996aa6d7f0
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-16 21:24:49 -0800

    AECP Address Access Mode definitions completed

--- include/jdksavdecc_aecp_aa.h
+++ include/jdksavdecc_aecp_aa.h
@@ -41,7 +41,7 @@ extern "C" {
 /** \addtogroup aecp AECP - Clause 9 */
 /*@{*/
 
-/** \addtogroup aecpdu_aa AECPDU Address Access - Clause 9.2.1.3 */
+/** \addtogroup AECP_AA AECP Address Access Offsets - Clause 9.2.1.3 */
 /*@{*/
 
 #define JDKSAVDECC_AECPDU_AA_OFFSET_CONTROLLER_ENTITY_ID (JDKSAVDECC_COMMON_CONTROL_HEADER_LEN + 0)
@@ -49,7 +49,7 @@ extern "C" {
 #define JDKSAVDECC_AECPDU_AA_OFFSET_TLV_COUNT (JDKSAVDECC_COMMON_CONTROL_HEADER_LEN + 10)
 #define JDKSAVDECC_AECPDU_AA_LEN (JDKSAVDECC_COMMON_CONTROL_HEADER_LEN + 12)
 
-/** \addtogroup aecpdu_aa_tlv AECPDU Address Access TLV - Clause 9.2.1.3.3 */
+/** \addtogroup AECP_AA_tlv AECP Address Access TLV Offsets - Clause 9.2.1.3.3 */
 /*@{*/
 
 #define JDKSAVDECC_AECPDU_AA_TLV_OFFSET_MODE_LENGTH (0)
@@ -59,11 +59,39 @@ extern "C" {
 
 /*@}*/
 
-/** \addtogroup aecpdu_aa AECPDU Address Access - Clause 9.2.1.3 */
+/** \addtogroup AECP_AA_mode AECP Address Access mode - Clause 9.2.1.3.3 */
+/*@{*/
+
+#define JDKSAVDECC_AECP_AA_MODE_READ (0)
+#define JDKSAVDECC_AECP_AA_MODE_WRITE (1)
+#define JDKSAVDECC_AECP_AA_MODE_EXECUTE (2)
+
+/*@}*/
+
+/** \addtogroup AECP_AA_tlv AECP Address Access TLV Status - Clause 9.2.1.3.4 */
+/*@{*/
+
+#define JDKSAVDECC_AECP_AA_STATUS_SUCCESS (0)
+#define JDKSAVDECC_AECP_AA_STATUS_NOT_IMPLEMENTED (1)
+#define JDKSAVDECC_AECP_AA_STATUS_ADDRESS_TOO_LOW (2)
+#define JDKSAVDECC_AECP_AA_STATUS_ADDRESS_TOO_HIGH (3)
+#define JDKSAVDECC_AECP_AA_STATUS_ADDRESS_INVALID (4)
+#define JDKSAVDECC_AECP_AA_STATUS_TLV_INVALID (5)
+#define JDKSAVDECC_AECP_AA_STATUS_DATA_INVALID (6)
+#define JDKSAVDECC_AECP_AA_STATUS_UNSUPPORTED (7)
+
+/*@}*/
+
+/** \addtogroup AECP_AA_timeouts AECP Address Access timeouts - Clause 9.2.1.3.4 */
+/*@{*/
+#define JDKSAVDECC_AECP_AA_TIMEOUT_IN_MS (250)
+/*@}*/
+
+/** \addtogroup AECP_AA AECPDU Address Access - Clause 9.2.1.3 */
 /*@{*/
 
 /**
- * Extract the eui64 value of the controller_entity_id field of the AECPDU_AA
+ * Extract the eui64 value of the controller_entity_id field of the aecp_aa
  *object from a network buffer.
  *
  *
@@ -74,12 +102,12 @@ extern "C" {
  * @param pos offset from base to read the field from;
  * @return the struct jdksavdecc_eui64 controller_entity_id value
  */
-static inline struct jdksavdecc_eui64 jdksavdecc_aecpdu_aa_get_controller_entity_id(void const *base, ssize_t pos) {
+static inline struct jdksavdecc_eui64 jdksavdecc_aecp_aa_get_controller_entity_id(void const *base, ssize_t pos) {
     return jdksavdecc_eui64_get(base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_CONTROLLER_ENTITY_ID);
 }
 
 /**
- * Store a eui64 value to the controller_entity_id field of the AECPDU_AA object
+ * Store a eui64 value to the controller_entity_id field of the aecp_aa object
  *to a network buffer.
  *
  *
@@ -90,12 +118,12 @@ static inline struct jdksavdecc_eui64 jdksavdecc_aecpdu_aa_get_controller_entity
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_set_controller_entity_id(struct jdksavdecc_eui64 v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_set_controller_entity_id(struct jdksavdecc_eui64 v, void *base, ssize_t pos) {
     jdksavdecc_eui64_set(v, base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_CONTROLLER_ENTITY_ID);
 }
 
 /**
- * Extract the uint16 value of the sequence_id field of the AECPDU_AA object
+ * Extract the uint16 value of the sequence_id field of the aecp_aa object
  *from a network buffer.
  *
  *
@@ -106,12 +134,12 @@ static inline void jdksavdecc_aecpdu_aa_set_controller_entity_id(struct jdksavde
  * @param pos offset from base to read the field from;
  * @return the uint16_t sequence_id value
  */
-static inline uint16_t jdksavdecc_aecpdu_aa_get_sequence_id(void const *base, ssize_t pos) {
+static inline uint16_t jdksavdecc_aecp_aa_get_sequence_id(void const *base, ssize_t pos) {
     return jdksavdecc_uint16_get(base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_SEQUENCE_ID);
 }
 
 /**
- * Store a uint16 value to the sequence_id field of the AECPDU_AA object to a
+ * Store a uint16 value to the sequence_id field of the aecp_aa object to a
  *network buffer.
  *
  *
@@ -122,12 +150,12 @@ static inline uint16_t jdksavdecc_aecpdu_aa_get_sequence_id(void const *base, ss
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_set_sequence_id(uint16_t v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_set_sequence_id(uint16_t v, void *base, ssize_t pos) {
     jdksavdecc_uint16_set(v, base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_SEQUENCE_ID);
 }
 
 /**
- * Extract the uint16 value of the tlv_count field of the AECPDU_AA object from
+ * Extract the uint16 value of the tlv_count field of the aecp_aa object from
  *a network buffer.
  *
  *
@@ -138,12 +166,12 @@ static inline void jdksavdecc_aecpdu_aa_set_sequence_id(uint16_t v, void *base, 
  * @param pos offset from base to read the field from;
  * @return the uint16_t tlv_count value
  */
-static inline uint16_t jdksavdecc_aecpdu_aa_get_tlv_count(void const *base, ssize_t pos) {
+static inline uint16_t jdksavdecc_aecp_aa_get_tlv_count(void const *base, ssize_t pos) {
     return jdksavdecc_uint16_get(base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_TLV_COUNT);
 }
 
 /**
- * Store a uint16 value to the tlv_count field of the AECPDU_AA object to a
+ * Store a uint16 value to the tlv_count field of the aecp_aa object to a
  *network buffer.
  *
  *
@@ -154,17 +182,17 @@ static inline uint16_t jdksavdecc_aecpdu_aa_get_tlv_count(void const *base, ssiz
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_set_tlv_count(uint16_t v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_set_tlv_count(uint16_t v, void *base, ssize_t pos) {
     jdksavdecc_uint16_set(v, base, pos + JDKSAVDECC_AECPDU_AA_OFFSET_TLV_COUNT);
 }
 
 /*@}*/
 
-/** \addtogroup aecpdu_aa AECPDU Address Access - Clause 9.2.1.3 */
+/** \addtogroup AECP_AA AECPDU Address Access - Clause 9.2.1.3 */
 /*@{*/
 
 /// AECPDU Address Access - Clause 9.2.1.3
-struct jdksavdecc_aecpdu_aa {
+struct jdksavdecc_aecp_aa {
     struct jdksavdecc_aecpdu_common aecpdu_header;
     struct jdksavdecc_eui64 controller_entity_id;
     uint16_t sequence_id;
@@ -172,58 +200,58 @@ struct jdksavdecc_aecpdu_aa {
 };
 
 /**
- * Extract the jdksavdecc_aecpdu_aa_t structure from a network buffer.
+ * Extract the jdksavdecc_AECP_AA_t structure from a network buffer.
  *
  *
  *
  * Bounds checking of the buffer size is done.
  *
- * @param p pointer to aecpdu_aa structure to fill in.
+ * @param p pointer to AECP_AA structure to fill in.
  * @param base pointer to raw memory buffer to read from.
  * @param pos offset from base to read the field from;
  * @param len length of the raw memory buffer;
  * @return -1 if the buffer length is insufficent, otherwise the offset of the
  *octet following the structure in the buffer.
  */
-static inline ssize_t jdksavdecc_aecpdu_aa_read(struct jdksavdecc_aecpdu_aa *p, void const *base, ssize_t pos, size_t len) {
+static inline ssize_t jdksavdecc_AECP_AA_read(struct jdksavdecc_aecp_aa *p, void const *base, ssize_t pos, size_t len) {
     ssize_t r = jdksavdecc_validate_range(pos, len, JDKSAVDECC_AECPDU_AA_LEN);
     if (r >= 0) {
         jdksavdecc_aecpdu_common_read(&p->aecpdu_header, base, pos, len);
-        p->controller_entity_id = jdksavdecc_aecpdu_aa_get_controller_entity_id(base, pos);
-        p->sequence_id = jdksavdecc_aecpdu_aa_get_sequence_id(base, pos);
-        p->tlv_count = jdksavdecc_aecpdu_aa_get_tlv_count(base, pos);
+        p->controller_entity_id = jdksavdecc_aecp_aa_get_controller_entity_id(base, pos);
+        p->sequence_id = jdksavdecc_aecp_aa_get_sequence_id(base, pos);
+        p->tlv_count = jdksavdecc_aecp_aa_get_tlv_count(base, pos);
     }
     return r;
 }
 
 /**
- * Store the jdksavdecc_aecpdu_aa_t structure to a network buffer.
+ * Store the jdksavdecc_aecp_aa structure to a network buffer.
  *
  *
  *
  * Bounds checking of the buffer size is done.
  *
- * @param p const pointer to aecpdu_aa structure to read from.
+ * @param p const pointer to AECP_AA structure to read from.
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  * @param len length of the raw memory buffer;
  * @return -1 if the buffer length is insufficent, otherwise the offset of the
  *octet following the structure in the buffer.
  */
-static inline ssize_t jdksavdecc_aecpdu_aa_write(struct jdksavdecc_aecpdu_aa const *p, void *base, size_t pos, size_t len) {
+static inline ssize_t jdksavdecc_aecp_aa_write(struct jdksavdecc_aecp_aa const *p, void *base, size_t pos, size_t len) {
     ssize_t r = jdksavdecc_validate_range(pos, len, JDKSAVDECC_AECPDU_AA_LEN);
     if (r >= 0) {
         jdksavdecc_aecpdu_common_write(&p->aecpdu_header, base, pos, len);
-        jdksavdecc_aecpdu_aa_set_controller_entity_id(p->controller_entity_id, base, pos);
-        jdksavdecc_aecpdu_aa_set_sequence_id(p->sequence_id, base, pos);
-        jdksavdecc_aecpdu_aa_set_tlv_count(p->tlv_count, base, pos);
+        jdksavdecc_aecp_aa_set_controller_entity_id(p->controller_entity_id, base, pos);
+        jdksavdecc_aecp_aa_set_sequence_id(p->sequence_id, base, pos);
+        jdksavdecc_aecp_aa_set_tlv_count(p->tlv_count, base, pos);
     }
     return r;
 }
 
 /*@}*/
 
-/** \addtogroup aecpdu_aa_tlv AECPDU Address Access TLV - Clause 9.2.1.3.3 */
+/** \addtogroup aecp_aa_tlv AECPDU Address Access TLV - Clause 9.2.1.3.3 */
 /*@{*/
 
 /**
@@ -238,7 +266,7 @@ static inline ssize_t jdksavdecc_aecpdu_aa_write(struct jdksavdecc_aecpdu_aa con
  * @param pos offset from base to read the field from;
  * @return the uint16_t mode_length value
  */
-static inline uint16_t jdksavdecc_aecpdu_aa_tlv_get_mode_length(void const *base, ssize_t pos) {
+static inline uint16_t jdksavdecc_aecp_aa_tlv_get_mode_length(void const *base, ssize_t pos) {
     return jdksavdecc_uint16_get(base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_MODE_LENGTH);
 }
 
@@ -254,7 +282,7 @@ static inline uint16_t jdksavdecc_aecpdu_aa_tlv_get_mode_length(void const *base
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_tlv_set_mode_length(uint16_t v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_tlv_set_mode_length(uint16_t v, void *base, ssize_t pos) {
     jdksavdecc_uint16_set(v, base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_MODE_LENGTH);
 }
 
@@ -270,7 +298,7 @@ static inline void jdksavdecc_aecpdu_aa_tlv_set_mode_length(uint16_t v, void *ba
  * @param pos offset from base to read the field from;
  * @return the uint32_t address_upper value
  */
-static inline uint32_t jdksavdecc_aecpdu_aa_tlv_get_address_upper(void const *base, ssize_t pos) {
+static inline uint32_t jdksavdecc_aecp_aa_tlv_get_address_upper(void const *base, ssize_t pos) {
     return jdksavdecc_uint32_get(base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_ADDRESS_UPPER);
 }
 
@@ -286,7 +314,7 @@ static inline uint32_t jdksavdecc_aecpdu_aa_tlv_get_address_upper(void const *ba
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_tlv_set_address_upper(uint32_t v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_tlv_set_address_upper(uint32_t v, void *base, ssize_t pos) {
     jdksavdecc_uint32_set(v, base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_ADDRESS_UPPER);
 }
 
@@ -302,7 +330,7 @@ static inline void jdksavdecc_aecpdu_aa_tlv_set_address_upper(uint32_t v, void *
  * @param pos offset from base to read the field from;
  * @return the uint32_t address_lower value
  */
-static inline uint32_t jdksavdecc_aecpdu_aa_tlv_get_address_lower(void const *base, ssize_t pos) {
+static inline uint32_t jdksavdecc_aecp_aa_tlv_get_address_lower(void const *base, ssize_t pos) {
     return jdksavdecc_uint32_get(base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_ADDRESS_LOWER);
 }
 
@@ -318,17 +346,17 @@ static inline uint32_t jdksavdecc_aecpdu_aa_tlv_get_address_lower(void const *ba
  * @param base pointer to raw memory buffer to write to.
  * @param pos offset from base to write the field to;
  */
-static inline void jdksavdecc_aecpdu_aa_tlv_set_address_lower(uint32_t v, void *base, ssize_t pos) {
+static inline void jdksavdecc_aecp_aa_tlv_set_address_lower(uint32_t v, void *base, ssize_t pos) {
     jdksavdecc_uint32_set(v, base, pos + JDKSAVDECC_AECPDU_AA_TLV_OFFSET_ADDRESS_LOWER);
 }
 
 /*@}*/
 
-/** \addtogroup aecpdu_aa_tlv AECPDU Address Access TLV - Clause 9.2.1.3.3 */
+/** \addtogroup aecp_aa_tlv AECPDU Address Access TLV - Clause 9.2.1.3.3 */
 /*@{*/
 
 /// AECPDU Address Access TLV - Clause 9.2.1.3.3
-struct jdksavdecc_aecpdu_aa_tlv {
+struct jdksavdecc_aecp_aa_tlv {
     uint16_t mode_length;
     uint32_t address_upper;
     uint32_t address_lower;
@@ -348,13 +376,12 @@ struct jdksavdecc_aecpdu_aa_tlv {
  * @return -1 if the buffer length is insufficent, otherwise the offset of the
  *octet following the structure in the buffer.
  */
-static inline ssize_t
-jdksavdecc_aecpdu_aa_tlv_read(struct jdksavdecc_aecpdu_aa_tlv *p, void const *base, ssize_t pos, size_t len) {
+static inline ssize_t jdksavdecc_aecp_aa_tlv_read(struct jdksavdecc_aecp_aa_tlv *p, void const *base, ssize_t pos, size_t len) {
     ssize_t r = jdksavdecc_validate_range(pos, len, JDKSAVDECC_AECPDU_AA_TLV_LEN);
     if (r >= 0) {
-        p->mode_length = jdksavdecc_aecpdu_aa_tlv_get_mode_length(base, pos);
-        p->address_upper = jdksavdecc_aecpdu_aa_tlv_get_address_upper(base, pos);
-        p->address_lower = jdksavdecc_aecpdu_aa_tlv_get_address_lower(base, pos);
+        p->mode_length = jdksavdecc_aecp_aa_tlv_get_mode_length(base, pos);
+        p->address_upper = jdksavdecc_aecp_aa_tlv_get_address_upper(base, pos);
+        p->address_lower = jdksavdecc_aecp_aa_tlv_get_address_lower(base, pos);
     }
     return r;
 }
@@ -373,13 +400,12 @@ jdksavdecc_aecpdu_aa_tlv_read(struct jdksavdecc_aecpdu_aa_tlv *p, void const *ba
  * @return -1 if the buffer length is insufficent, otherwise the offset of the
  *octet following the structure in the buffer.
  */
-static inline ssize_t
-jdksavdecc_aecpdu_aa_tlv_write(struct jdksavdecc_aecpdu_aa_tlv const *p, void *base, size_t pos, size_t len) {
+static inline ssize_t jdksavdecc_aecp_aa_tlv_write(struct jdksavdecc_aecp_aa_tlv const *p, void *base, size_t pos, size_t len) {
     ssize_t r = jdksavdecc_validate_range(pos, len, JDKSAVDECC_AECPDU_AA_TLV_LEN);
     if (r >= 0) {
-        jdksavdecc_aecpdu_aa_tlv_set_mode_length(p->mode_length, base, pos);
-        jdksavdecc_aecpdu_aa_tlv_set_address_upper(p->address_upper, base, pos);
-        jdksavdecc_aecpdu_aa_tlv_set_address_lower(p->address_lower, base, pos);
+        jdksavdecc_aecp_aa_tlv_set_mode_length(p->mode_length, base, pos);
+        jdksavdecc_aecp_aa_tlv_set_address_upper(p->address_upper, base, pos);
+        jdksavdecc_aecp_aa_tlv_set_address_lower(p->address_lower, base, pos);
     }
     return r;
 }
